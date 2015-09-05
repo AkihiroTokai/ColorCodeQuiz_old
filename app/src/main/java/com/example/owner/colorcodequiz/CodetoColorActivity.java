29a6f4a98f1555b472a9fe06aee25ec14b51fdc5
ToukaiAkihiro <tokaimas@yahoo.co.jp>
2015-09-05 22:28:20 +0900

    CodeClean

--- app/src/main/java/com/example/owner/colorcodequiz/CodetoColorActivity.java
+++ app/src/main/java/com/example/owner/colorcodequiz/CodetoColorActivity.java
@@ -314,6 +314,7 @@ public class CodetoColorActivity extends AppCompatActivity {
         }
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
